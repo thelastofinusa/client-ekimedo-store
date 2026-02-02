3fdf092ebf23a58ef16442d57f980a412710bcfd
Holiday <thelastofinusa@gmail.com>
2026-02-02 20:10:12 +0100

    Update hero.comp.tsx

--- app/(routes)/(index)/_components/hero.comp.tsx
+++ app/(routes)/(index)/_components/hero.comp.tsx
@@ -4,20 +4,24 @@ import Link from "next/link";
 import { Route } from "next";
 import { motion } from "motion/react";
 
+import Image from "next/image";
 import { siteConfig } from "@/site.config";
 import { buttonVariants } from "@/ui/button";
 
 export const HeroComp = () => {
   return (
     <section className="bg-charcoal relative flex min-h-screen items-center justify-center overflow-hidden pt-20">
-      {/* Fixed background */}
-      <div
-        className="fixed inset-0 -z-10 bg-cover bg-center"
-        style={{ backgroundImage: "url('/hero.avif')" }}
-      />
-
-      {/* Overlay */}
-      <div className="bg-foreground/80 fixed inset-0 -z-10 backdrop-blur-xs" />
+      <div className="absolute inset-0">
+        <Image
+          src="/hero.avif"
+          alt="Background"
+          fill
+          priority
+          quality={100}
+          className="bg-cover bg-fixed bg-center object-cover"
+        />
+        <div className="bg-foreground/80 absolute inset-0 backdrop-blur-xs" />
+      </div>
 
       <div className="text-background relative z-10 mx-auto w-full max-w-6xl px-6 md:px-12">
         <div className="flex flex-col items-center gap-4 text-center sm:gap-6 md:gap-12">
